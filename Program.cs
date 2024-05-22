68c599535b7e516f9f1e34424dcecf1b406585b4
MegaJaba <146406618+MegaJaba@users.noreply.github.com>
2024-05-22 22:54:19 +0700

    Add files via upload
    подписка на событие

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace event_ {
+    internal class Program {
+    // Событие
+    public static event EventHandler<UserEnteredNumberEventArgs> UserEnteredNumber;
+    // Функция вывода числа введенного пользователем
+    private static void PrintUserEnteredNumber(object sender, UserEnteredNumberEventArgs e) {
+        Console.WriteLine($"\nПользователь ввел число {e.Input} в {e.EnteredAt}\n");
+    }
+    static void Main(string[] args) {
+        string input;
+        int number;
+
+        // Подписка на событие
+        UserEnteredNumber += PrintUserEnteredNumber;
+
+        while (true) {
+            Console.Write("Введите число > ");
+
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out number)) {
+                // Вызов события
+                UserEnteredNumber?.Invoke(null, new UserEnteredNumberEventArgs { Input = number, EnteredAt = DateTime.Now });
+            }
+        }
+    }
+    // Объект аргументов события
+    public class UserEnteredNumberEventArgs {
+        public int Input { get; set; }
+        public DateTime EnteredAt { get; set; }
+    }
+}
+}
